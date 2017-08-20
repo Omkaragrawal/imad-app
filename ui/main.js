b700a68ef582654cd87524ec8cb702fa86b228e3
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2017-08-20 16:52:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,12 +2,18 @@ console.log('Loaded!');
 
 var submit = document.getElementById('submit');
 submit.onclick = function() {
-    
-
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-console.log(username);
-console.log(password);
-request.open('POST', 'http://omkar3654.imad.hasura-app.io/login');
-request.send(JSON.stringify({username: username, password: password}));
+    var request = new XHLHttpRequest();
+    request.onreadystateChange = function() {
+        if(request.readyState === XHLHttpRequest.done) {
+           if(request.statues === 200) {
+               
+           } 
+        }
+    };
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://omkar3654.imad.hasura-app.io/login');
+    request.send(JSON.stringify({username: username, password: password}));
 };
